66abee6750fcef6a783124c8c418658e5e58d009
KennethMcLachlan <kennethmclachlan11@gmail.com>
2024-11-05 19:09:57 -0800

    Implemented run in background patch

---
+++ Assets/Scripts/AppSettings.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppSettings : MonoBehaviour
+{
+    void Start()
+    {
+        Application.runInBackground = true;
+    }
+
+}
